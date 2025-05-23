843c31bb3e298e64128c02c5af8f00d887c27c7d
Martin Zavadzan <martin.zavadzan@vacuumlabs.com>
2025-05-23 12:55:16 +0200

    initial privy setup

---
+++ src/app/providers.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { PrivyProvider } from "@privy-io/react-auth"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode, useState } from "react"
+import { WagmiProvider } from "@privy-io/wagmi"
+import { wagmiConfig } from "../configs/wagmi"
+
+type Props = {
+  children: ReactNode
+}
+
+export const Providers = ({ children }: Props) => {
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+      <PrivyProvider
+        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
+        config={{
+          embeddedWallets: {
+            createOnLogin: "all-users",
+          },
+          loginMethods: ["email", "wallet"],
+          appearance: {
+            walletList: ["metamask", "phantom", "rabby_wallet", "rainbow"],
+            theme: "#121419",
+            accentColor: "#FE7E3A",
+            loginMessage: "Welcome to SanBoxFi!",
+          },
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
+        </QueryClientProvider>
+      </PrivyProvider>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { ConnectWalletButton } from "./connect-wallet-button"
+
+export const Header = ( ) => {
+    return <div>
+        <ConnectWalletButton />
+    </div>
+}
