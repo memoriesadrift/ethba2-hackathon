23933c5740ef95ef31b7c45c643e4273d5a386d2
Ladislav Dubravsky <dubravsky90@gmail.com>
2025-05-24 15:37:00 +0200

    contracts: lido clone deployment

--- contracts/test/setup-local.sh
+++ contracts/test/setup-local.sh
@@ -19,5 +19,7 @@ forge script script/deploy/Uniswapv2.s.sol:Deploy \
     --rpc-url $RPC_URL --private-key $ANVIL1_PRIVKEY --broadcast
 forge script script/deploy/Aave.s.sol:Deploy \
     --rpc-url $RPC_URL --private-key $ANVIL1_PRIVKEY --broadcast
+forge script script/deploy/Lido.s.sol:Deploy \
+   --rpc-url $RPC_URL --private-key $ANVIL1_PRIVKEY --broadcast
 
 wait $ANVIL_PID
